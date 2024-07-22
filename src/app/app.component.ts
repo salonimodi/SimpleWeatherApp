61e7335fab2e6b3d29010cdb91e9c72732a46224
Saloni Modi <salonimodi@Salonis-MBP.lan>
2024-07-22 09:18:55 -0700

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SearchCityComponent } from './search-city/search-city.component';
 import { HeaderComponent } from './header/header.component';
 import { NextHourComponent } from "./next-hour/next-hour.component";
 import { DynamicTabsComponent } from "./dynamic-tabs/dynamic-tabs.component";
@@ -9,7 +8,6 @@ import { DynamicTabsComponent } from "./dynamic-tabs/dynamic-tabs.component";
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, 
-    SearchCityComponent,
     HeaderComponent, 
     NextHourComponent, 
     DynamicTabsComponent],
