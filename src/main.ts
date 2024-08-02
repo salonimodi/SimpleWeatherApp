a6afa3e34822c52209d0805c8b49a4af81940539
Saloni Modi <salonimodi@Salonis-MacBook-Pro.local>
2024-08-02 12:09:31 -0700

    bug fixes

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule),
-    provideAnimations(), provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimationsAsync(),
   ]}
   ).then()
   .catch((err) => console.error(err));
